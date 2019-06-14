9313176d82918db09c1e4cc9c33d7f41142fb8c7
cindyxlee <48038386+cindyxlee@users.noreply.github.com>
2019-06-13 18:56:04 -0700

    Create quizdata.js

---
+++ quizdata.js
@@ -0,0 +1,112 @@
+const quizData = [
+  {
+    question: 'What is the oldest U.S. national park?',
+    answers: [
+      'Great Smoky Mountains National Park',
+      'Grand Canyon National Park',
+      'Yellowstone National Park',
+      'Big Bend National Park'
+    ],
+    correctAnswer: 'Yellowstone National Park',
+    fact: 'The first national park was founded in 1872'
+  },
+  {
+    question: 'Which national park is home to the world\'s tallest tree?',
+    answers: [
+      'Redwood National Park',
+      'Sequoia National Park',
+      'Everglades National Park',
+      'Cuyahoga Valley National Park'
+    ],
+    correctAnswer: 'Redwood National Park',
+    fact: 'Named Hyperion, it is recognized by the Guinness Book of World Records'
+  },
+  {
+    question: 'Which state has the most national parks?',
+    answers: [
+      'Alaska',
+      'Utah',
+      'Montana',
+      'California'
+    ],
+    correctAnswer: 'California',
+    fact: 'Alaska and California used to be tied with eight each until Pinnacles became a national park in CA in 2013'
+  },
+  {
+    question: 'Which is the smallest national park?',
+    answers: [
+      'Isle Royale National Park',
+      'Hot Springs National Park',
+      'Katmai National Park',
+      'Pinnacles National Park'
+    ],
+    correctAnswer: 'Hot Springs National Park',
+    fact: 'It is about 5500 acres and the first federally protected piece of land in 1832'
+  },
+  {
+    question: 'What animal killed a man in 2010 in Olympic National Park?',
+    answers: [
+      'Elk',
+      'Bear',
+      'Goat',
+      'Cougar'
+    ],
+    correctAnswer: 'Goat',
+    fact: 'These goats are not a native species to the region'
+  },
+  {
+    question: 'Which national park was created during the Great Depression?',
+    answers: [
+      'Shenandoah National Park',
+      'Gates of the Artic National Park',
+      'Petrified Forest National Park',
+      'Dry Tortugas National Park'
+    ],
+    correctAnswer: 'Shenandoah National Park',
+    fact: 'It was built by members of the Civilian Conservation Corps'
+  },
+  {
+    question: 'Which national park is one of the Seven Natural Wonders of the World?',
+    answers: [
+      'Yosemite National Park',
+      'Zion National Park',
+      'Grand Canyon National Park',
+      'Sequoia National Park'
+    ],
+    correctAnswer: 'Grand Canyon National Park',
+    fact: 'However, it was previously called the Big Canyon or the Great Canyon'
+  },
+  {
+    question: 'The National Park Services protects over __ ________ acres of wild landscapes and historic sites.',
+    answers: [
+      '42 million',
+      '84 million',
+      '239 million',
+      '171 million'
+    ],
+    correctAnswer: '84 million',
+    fact: 'Parks rangers also educate visitors about park history, natural features, wildlife, and conservation'
+  },
+  {
+    question: 'Which national park recorded one of the hottest temperatures on earth in 1913?',
+    answers: [
+      'Death Valley National Park',
+      'Badlands National Park',
+      'Hawaii Volcanoes National Park',
+      'Mesa Verde National Park'
+    ],
+    correctAnswer: 'Death Valley National Park',
+    fact: 'The temperature registered at 134 degrees'
+  },
+  {
+    question: 'What national park is the most visited every year?',
+    answers: [
+      'Yellowstone National Park',
+      'Mount Ranier National Park',
+      'Grand Canyon National Park',
+      'Great Smoky Mountains National Park'
+    ],
+    correctAnswer: 'Great Smoky Mountains National Park',
+    fact: 'More than 9 million guests visit every year'
+  }
+];
